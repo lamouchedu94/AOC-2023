14f4c3730f87dc5304acf6ef3d55df244227115a
lamouchedu94 <lamouchedu54@gmail.com>
2023-12-04 11:25:49 +0100

    [ADD] Get game id

--- day2/step1/main.go
+++ day2/step1/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	tab, err := read("input.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var res int
+	fmt.Sscanf(tab[0], "Game %d", &res)
+	fmt.Println(res)
+}
+
+func read(path string) ([]string, error) {
+	var tab []string
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	test := bufio.NewScanner(file)
+	test.Split(bufio.ScanLines)
+
+	for test.Scan() {
+		tab = append(tab, test.Text())
+	}
+	return tab, err
+
+}
