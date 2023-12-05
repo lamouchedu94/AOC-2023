941360509a2a929f19ca21a1e0a291c44312859f
lamouchedu94 <lamouchedu54@gmail.com>
2023-12-05 09:50:02 +0100

    [ADD] Struct + print

--- day2/step1/main.go
+++ day2/step1/main.go
@@ -4,17 +4,50 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
+type result struct {
+	id    int
+	red   int
+	blue  int
+	green int
+}
+
 func main() {
-	tab, err := read("input.txt")
+	tab, err := read("../input.txt")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	var res int
-	fmt.Sscanf(tab[0], "Game %d", &res)
-	fmt.Println(res)
+	all_party := []result{}
+	for _, line := range tab {
+		var party result
+		new_line := strings.Split(line, ":")
+		fmt.Sscanf(new_line[0], "Game %d", &party.id)
+		data := strings.Split(new_line[1], ";")
+		for _, value := range data {
+			game := strings.Split(string(value), ",")
+			for _, car := range game {
+				var number int
+				var color string
+				fmt.Sscanf(car, "%d %s", &number, &color)
+				switch color {
+				case "blue":
+					party.blue += number
+				case "red":
+					party.red += number
+				case "green":
+					party.green += number
+				}
+
+			}
+		}
+		all_party = append(all_party, party)
+	}
+	for _, val := range all_party {
+		fmt.Printf("%+v\n", val)
+	}
 }
 
 func read(path string) ([]string, error) {
